ec5531515bd6fb43b2522ed18c6493fd619ad505
Kenneth Ge <kenge@student.edgemont.org>
2021-07-05 17:47:31 -0400

    added example server

---
+++ hacking_server.py
@@ -0,0 +1,21 @@
+# We will need the following module to generate randomized lost packets
+from socket import *
+
+# Create a UDP socket
+# Notice the use of SOCK_DGRAM for UDP packets
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Assign IP address and port number to socket
+serverSocket.bind(('', 12000))
+
+while True:
+    # Receive the client packet along with the address it is coming from
+    message, address = serverSocket.recvfrom(1024)
+
+    message = message.decode()
+    print(message)
+
+    if message == 'get passwords':
+        serverSocket.sendto('password'.encode(), address)
+    else:
+        serverSocket.sendto('forbidden'.encode(), address)
